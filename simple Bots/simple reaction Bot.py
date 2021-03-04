38be09bb5f25ae243a5711dd480dcd8c242ec2ce
Jonathan <mansfeldergeorg@gmailcom>
2021-03-04 11:52:46 +0100

    added explanations

--- simple Bots/simple reaction Bot.py
+++ simple Bots/simple reaction Bot.py
@@ -1,16 +1,16 @@
 import discord
 from discord.ext import commands
 
-bot = commands.bot(command_prefix= ",")
+bot = commands.Bot(command_prefix= ",")
 
-@bot.event()
+@bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@bot.event()
-async def on_message(message):
-    if message.content.startswith("Hello"):
-        await message.channel.send(f"Hello {message.author.mention}")
+@bot.event
+async def on_message(message): #if a user sends a message
+    if message.content.startswith("Hello"): #if the message starts with "Hello"
+        await message.channel.send(f"Hello {message.author.mention}") #send message "Hello" and mention the user
 
 
 @bot.run('TOKEN')
